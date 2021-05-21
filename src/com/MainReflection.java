db9aa8c200f05de5a14096723f5d70a787159d8f
ADivaev <ADivaev@bellintegrator.com>
2021-05-21 16:32:50 +0500

    Collection introduce

--- src/com/MainReflection.java
+++ src/com/MainReflection.java
@@ -1,6 +1,8 @@
 package com;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import com.urise.webapp.model.Resume;
 
@@ -10,7 +12,7 @@ import com.urise.webapp.model.Resume;
  * @author ADivaev
  */
 public class MainReflection {
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
         Resume r = new Resume();
         Field field = r.getClass().getDeclaredFields()[0];
         field.setAccessible(true);
@@ -18,6 +20,8 @@ public class MainReflection {
         System.out.println(field.get(r));
         field.set(r,"new_uuid");
         // TODO : invoke r.toString via reflection
-        System.out.println(r);
+        //System.out.println(r);
+        Method m = r.getClass().getMethod("toString");
+        m.invoke(r);
     }
 }
