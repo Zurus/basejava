4c35e6c2dcedf5affb3d2be893302724daef13e3
ADivaev <ADivaev@bellintegrator.com>
2021-05-25 11:11:01 +0500

    bug fixes

--- test/com/urise/webapp/storage/AbstractStorageTest.java
+++ test/com/urise/webapp/storage/AbstractStorageTest.java
@@ -20,7 +20,7 @@ public abstract class AbstractStorageTest {
     private static final String UUID_1 = "uuid1";
     private static final String UUID_2 = "uuid2";
     private static final String UUID_3 = "uuid3";
-    private static final String UUID_4 = "uuid3";
+    private static final String UUID_4 = "uuid4";
 
     private static final Resume RESUME_1;
     private static final Resume RESUME_2;
@@ -97,20 +97,20 @@ public abstract class AbstractStorageTest {
     }
 
 
-//    @Test(expected = NotExistStorageException.class)
-//    public void delete() {
-//        storage.delete(UUID_1);
-//        assertSize(2);
-//        storage.get(UUID_1);
-//    }
-
-    @Test
+    @Test(expected = NotExistStorageException.class)
     public void delete() {
-        storage.delete(UUID_3);
-        Resume[] expectedArray = {new Resume(UUID_1),
-                new Resume(UUID_2)};
-        Assert.assertTrue(Arrays.equals(expectedArray, storage.getAll()));
+        storage.delete(UUID_1);
+        assertSize(2);
+        storage.get(UUID_1);
     }
+
+//    @Test
+//    public void delete() {
+//        storage.delete(UUID_3);
+//        Resume[] expectedArray = {new Resume(UUID_1),
+//                new Resume(UUID_2)};
+//        Assert.assertTrue(Arrays.equals(expectedArray, storage.getAll()));
+//    }
 
     @Test(expected = NotExistStorageException.class)
     public void getNotExist() {
