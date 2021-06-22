0554167822d64b0e085fb0022a9e37dd0831d64c
Zurus <Zulkund@mail.ru>
2021-06-22 15:08:07 +0300

    Map Storage implementation

--- test/com/urise/webapp/storage/MapUuidStorageTest.java
+++ test/com/urise/webapp/storage/MapUuidStorageTest.java
@@ -1,8 +1,5 @@
 package com.urise.webapp.storage;
 
-import org.junit.Ignore;
-
-@Ignore
 public class MapUuidStorageTest extends AbstractStorageTest {
     public MapUuidStorageTest() {
         super(new MapUuidStorage());
