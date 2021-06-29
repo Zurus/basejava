2730d45e06c823966859fe5c93575fc670b2a741
Divaev Artur <a.divaev.stc@innopolis.university>
2021-06-29 19:47:05 +0300

    File

---
+++ src/MainFile.java
@@ -0,0 +1,23 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainFile {
+    public static void main(String[] args) {
+        try
+        {
+            File file = new File(".\\.gitignore");
+            System.out.println(file.getCanonicalFile().toString());
+            File dir = new File("./src/com/urise/webapp");
+            System.out.println(dir.isDirectory());
+            String[] list = dir.list();
+            if (!Objects.isNull(list)) {
+                for(String name : list) {
+                    System.out.println(name);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
